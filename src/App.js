ab9bbf9f3b83938e1a5144962d5fd097489fb459
halima91 <belhajali.halima@gmail.com>
2019-01-18 11:42:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import puce from "./puce.jpg";
+import mastercard from "./mastercard.jpg";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIgloo } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faIgloo);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <div className="first-part">
+          <h1 className="title">Credit Card</h1>
+          <img
+            className="image-puce"
+            src={puce}
+            alt="puce-img"
+            height="20%"
+            width="30%"
+          />
+          <p className="number">7253 3256 7895 1245</p>
+          <span className="small-number">5422</span>
+          <p className="cardholder">cardholder</p>
+        </div>
+
+        <div className="second-part">
+          <h6>
+            VALID THRU
+            <FontAwesomeIcon icon="igloo" />
+          </h6>
+          <p className="valid">
+            MONTH/YEAR
+            <br />
+            11/50
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+          <img
+            className="mastercard"
+            src={mastercard}
+            alt="master-card"
+            height="0%"
+            width="30%"
+          />
+        </div>
       </div>
     );
   }
